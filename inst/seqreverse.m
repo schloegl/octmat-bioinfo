d4554fb864277a56c4569dbd4d3905ea1bf154e9
Alois Schloegl <alois.schloegl@ist.ac.at>
2018-01-08 20:03:19 +0100

    change coding style for compatibility to matlab and octave

--- inst/seqreverse.m
+++ inst/seqreverse.m
@@ -1,28 +1,28 @@
-## Copyright (C) 2008 Bill Denney
-##
-## This software is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or (at
-## your option) any later version.
-##
-## This software is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this software; see the file COPYING.  If not, see
-## <http://www.gnu.org/licenses/>.
+%% Copyright (C) 2008 Bill Denney
+%%
+%% This software is free software; you can redistribute it and/or modify it
+%% under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 3 of the License, or (at
+%% your option) any later version.
+%%
+%% This software is distributed in the hope that it will be useful, but
+%% WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%% General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this software; see the file COPYING.  If not, see
+%% <http://www.gnu.org/licenses/>.
 
-## -*- texinfo -*-
-## @deftypefn {Function File} {@var{seqr} =} seqreverse (@var{seqf})
-## Reverse a nucleotide sequence.  @var{seqf} can be either a numeric or
-## code DNA or RNA sequence or a struct with the field sequence, and it
-## will reverse the direction of the sequence.
-## @seealso{seqcomplement, seqrcomplement}
-## @end deftypefn
+%% -*- texinfo -*-
+%% @deftypefn {Function File} {@var{seqr} =} seqreverse (@var{seqf})
+%% Reverse a nucleotide sequence.  @var{seqf} can be either a numeric or
+%% code DNA or RNA sequence or a struct with the field sequence, and it
+%% will reverse the direction of the sequence.
+%% @seealso{seqcomplement, seqrcomplement}
+%% @end deftypefn
 
-## Author: Bill Denney <bill@denney.ws>
+%% Author: Bill Denney <bill@denney.ws>
 
 function s = seqreverse (seqf)
 
@@ -30,18 +30,19 @@ function s = seqreverse (seqf)
     s = seqf.sequence;
   else
     s = seqf;
-  endif
+  end
 
-  if (rows (s) != 1)
-    error ("seqreverse: the sequence must have a single row");
-  endif
+  if (size (s,1) ~= 1)
+    error ('seqreverse: the sequence must have a single row');
+  end
   s = fliplr (s);
   if isstruct (seqf)
     seqf.sequence = s;
     s = seqf;
-  endif
-endfunction
+  end
+end
 
-## Tests
-%!assert (seqreverse ("ATCG"), "GCTA")
-%!assert (seqreverse (struct ("sequence", "ATCG")), struct ("sequence", "GCTA"));
+%% Tests
+%!assert (isequal(seqreverse ('ATCG'), 'GCTA'))
+%!assert (isequal(seqreverse (struct ('sequence', 'ATCG')), struct ('sequence', 'GCTA')));
+
